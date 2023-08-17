485283bbbd156f00f94aec24e322a8c3962514ca
Vadym <trokhimchuk2003@gmail.com>
2023-08-17 09:25:02 +0300

    Added Code

---
+++ src/main/java/com/example/model/Applicant.java
@@ -0,0 +1,32 @@
+package com.example.model;
+
+public class Applicant {
+    private String firstName;
+    private String lastName;
+    private String faculty;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFaculty() {
+		return faculty;
+	}
+	public void setFaculty(String faculty) {
+		this.faculty = faculty;
+	}
+	@Override
+	public String toString() {
+		return "Applicant [firstName=" + firstName + ", lastName=" + lastName + ", faculty=" + faculty + "]";
+	}
+
+    
+}
+
